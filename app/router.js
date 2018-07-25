92f49a8555985239e1ac2d9b82d2483c3d54d267
mikeelemuel <mike.lemuelbacayo@gmail.com>
2018-07-25 18:34:32 +0800

    Include a route for organizations

--- app/router.js
+++ app/router.js
@@ -15,6 +15,7 @@ Router.map(function() {
       this.route('followers', { path: ':user_login/followers'}, function() { });
       this.route('orgs', { path: ':user_login/orgs'}, function() { });
     });
+    this.route('organizations');
   });
 });
 
