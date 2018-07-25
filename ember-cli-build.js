0687af6ce330618a255bbb480601be50d50f7f53
mikeelemuel <mike.lemuelbacayo@gmail.com>
2018-07-25 14:15:03 +0800

    Downloaded some addons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,12 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-font-awesome': {
+      useScss: true, // for ember-cli-sass
+      useLess: true  // for ember-cli-less
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
